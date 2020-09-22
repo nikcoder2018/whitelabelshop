af98c17fb8b9d668a582e967902df7b883df8b44
Nick Jay <b.nickjay05@gmail.com>
2020-09-22 23:23:40 +0800

    remove backoffice name

--- routes/web.php
+++ routes/web.php
@@ -19,63 +19,61 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/', 'DashboardController@index');
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    Route::resource('users/admins', 'AdminsController', ['except' => ['update', 'destroy']]);
+    Route::post('users/admins/update', 'AdminsController@update')->name('admins.update');
+    Route::post('users/admins/delete', 'AdminsController@destroy')->name('admins.destroy');
 
-    Route::prefix('backoffice')->group(function(){
-        Route::get('dashboard', 'DashboardController@index')->name('dashboard');
-        Route::resource('users/admins', 'AdminsController', ['except' => ['update', 'destroy']]);
-        Route::post('users/admins/update', 'AdminsController@update')->name('admins.update');
-        Route::post('users/admins/delete', 'AdminsController@destroy')->name('admins.destroy');
-
-        Route::resource('users/vendors', 'VendorsController', ['except' => ['update', 'destroy']]);
-        Route::post('users/vendors/update', 'VendorsController@update')->name('vendors.update');
-        Route::post('users/vendors/delete', 'VendorsController@destroy')->name('vendors.destroy');
+    Route::resource('users/vendors', 'VendorsController', ['except' => ['update', 'destroy']]);
+    Route::post('users/vendors/update', 'VendorsController@update')->name('vendors.update');
+    Route::post('users/vendors/delete', 'VendorsController@destroy')->name('vendors.destroy');
 
 
-        Route::resource('pages', 'PagesController', ['except' => ['update', 'destroy']]);
-        Route::post('pages/update', 'PagesController@update')->name('pages.update');
-        Route::post('pages/delete', 'PagesController@destroy')->name('pages.destroy');
+    Route::resource('pages', 'PagesController', ['except' => ['update', 'destroy']]);
+    Route::post('pages/update', 'PagesController@update')->name('pages.update');
+    Route::post('pages/delete', 'PagesController@destroy')->name('pages.destroy');
 
-        Route::resource('posts', 'PostsController', ['except' => ['update', 'destroy']]);
-        Route::post('posts/update', 'PostsController@update')->name('posts.update');
-        Route::post('posts/delete', 'PostsController@destroy')->name('posts.destroy');
+    Route::resource('posts', 'PostsController', ['except' => ['update', 'destroy']]);
+    Route::post('posts/update', 'PostsController@update')->name('posts.update');
+    Route::post('posts/delete', 'PostsController@destroy')->name('posts.destroy');
 
-        Route::resource('tags', 'TagsController', ['except' => ['update', 'destroy']]);
-        Route::post('tags/update', 'TagsController@update')->name('tags.update');
-        Route::post('tags/delete', 'TagsController@destroy')->name('tags.destroy');
+    Route::resource('tags', 'TagsController', ['except' => ['update', 'destroy']]);
+    Route::post('tags/update', 'TagsController@update')->name('tags.update');
+    Route::post('tags/delete', 'TagsController@destroy')->name('tags.destroy');
 
-        Route::resource('products', 'ProductsController', ['except' => ['update', 'destroy']]);
-        Route::post('products/update', 'ProductsController@update')->name('products.update');
-        Route::post('products/delete', 'ProductsController@destroy')->name('products.destroy');
-
-
-        Route::resource('categories', 'CategoriesController', ['except' => ['update', 'destroy']]);
-        Route::post('categories/update', 'CategoriesController@update')->name('categories.update');
-        Route::post('categories/delete', 'CategoriesController@destroy')->name('categories.destroy');
-
-        Route::resource('locations', 'LocationsController', ['except' => ['update', 'destroy']]);
-        Route::post('locations/update', 'LocationsController@update')->name('locations.update');
-        Route::post('locations/delete', 'LocationsController@destroy')->name('locations.destroy');
-
-        Route::resource('specialoffers', 'SpecialOffersController', ['except' => ['update', 'destroy']]);
-        Route::post('specialoffers/update', 'SpecialOffersController@update')->name('specialoffers.update');
-        Route::post('specialoffers/delete', 'SpecialOffersController@destroy')->name('specialoffers.destroy');
+    Route::resource('products', 'ProductsController', ['except' => ['update', 'destroy']]);
+    Route::post('products/update', 'ProductsController@update')->name('products.update');
+    Route::post('products/delete', 'ProductsController@destroy')->name('products.destroy');
 
 
-        Route::resource('attributes', 'AttributesController');
+    Route::resource('categories', 'CategoriesController', ['except' => ['update', 'destroy']]);
+    Route::post('categories/update', 'CategoriesController@update')->name('categories.update');
+    Route::post('categories/delete', 'CategoriesController@destroy')->name('categories.destroy');
 
-        
-        Route::prefix('json')->group(function(){
-            Route::get('all_categories', 'CategoriesController@getCategoriesJSON')->name('json.getcategories');
-            Route::post('get_category', 'CategoriesController@getCategoryDataJSON')->name('json.getcategorydata');
-            Route::post('get_vendor', 'VendorsController@getVendorDataJSON')->name('json.getvendordata');
-            Route::post('get_admin', 'AdminsController@getAdminDataJSON')->name('json.getadmindata');
-            Route::post('get_page', 'PagesController@getPageDataJSON')->name('json.getpagedata');
-            Route::post('get_post', 'PostsController@getPostDataJSON')->name('json.getpostdata');
-            Route::post('get_tag', 'TagsController@getTagDataJSON')->name('json.gettagdata');
-            Route::post('get_location', 'LocationsController@getLocationDataJSON')->name('json.getlocationdata');
-            Route::post('get_specialoffer', 'SpecialOffersController@getOfferDataJSON')->name('json.getspecialofferdata');
-        });
+    Route::resource('locations', 'LocationsController', ['except' => ['update', 'destroy']]);
+    Route::post('locations/update', 'LocationsController@update')->name('locations.update');
+    Route::post('locations/delete', 'LocationsController@destroy')->name('locations.destroy');
+
+    Route::resource('specialoffers', 'SpecialOffersController', ['except' => ['update', 'destroy']]);
+    Route::post('specialoffers/update', 'SpecialOffersController@update')->name('specialoffers.update');
+    Route::post('specialoffers/delete', 'SpecialOffersController@destroy')->name('specialoffers.destroy');
+
+
+    Route::resource('attributes', 'AttributesController');
+
+    
+    Route::prefix('json')->group(function(){
+        Route::get('all_categories', 'CategoriesController@getCategoriesJSON')->name('json.getcategories');
+        Route::post('get_category', 'CategoriesController@getCategoryDataJSON')->name('json.getcategorydata');
+        Route::post('get_vendor', 'VendorsController@getVendorDataJSON')->name('json.getvendordata');
+        Route::post('get_admin', 'AdminsController@getAdminDataJSON')->name('json.getadmindata');
+        Route::post('get_page', 'PagesController@getPageDataJSON')->name('json.getpagedata');
+        Route::post('get_post', 'PostsController@getPostDataJSON')->name('json.getpostdata');
+        Route::post('get_tag', 'TagsController@getTagDataJSON')->name('json.gettagdata');
+        Route::post('get_location', 'LocationsController@getLocationDataJSON')->name('json.getlocationdata');
+        Route::post('get_specialoffer', 'SpecialOffersController@getOfferDataJSON')->name('json.getspecialofferdata');
     });
+
 });
 
 Route::post('slugify', function(){
